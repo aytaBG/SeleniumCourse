1cc1c50d23031109aac0b1d1eaa03364ba314169
aytaBG <aytaB12@gmail.com>
2024-04-13 18:00:42 +0300

    Добавил 2 новых файла в 3_x

---
+++ 3_x/test_3_3_8.py
@@ -0,0 +1,53 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
+
+@pytest.fixture(scope='module')
+def browser():
+    browser = webdriver.Chrome()
+    browser.implicitly_wait(5)
+    yield browser
+    browser.quit()
+
+class TestRework:
+    def test_1(self, browser):
+
+        browser.get('https://suninjuly.github.io/registration1.html')
+
+        input1 = browser.find_element(By.CSS_SELECTOR, '.first_block .form-control.first')
+        input1.send_keys("Ivan")
+        input2 = browser.find_element(By.CSS_SELECTOR, '.first_block .form-control.second')
+        input2.send_keys("Petrov")
+        input3 = browser.find_element(By.CSS_SELECTOR, '.first_block .form-control.third')
+        input3.send_keys("Smolensk")
+
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        welcome_text = welcome_text_elt.text
+
+        assert welcome_text == "Congratulations! You have successfully registered!"
+
+    def test_2(self, browser):
+
+        browser.get('https://suninjuly.github.io/registration2.html')
+
+        input1 = browser.find_element(By.CSS_SELECTOR, '.first_block .form-control.first')
+        input1.send_keys("Ivan")
+        input2 = browser.find_element(By.CSS_SELECTOR, '.first_block .form-control.second')
+        input2.send_keys("Petrov")
+        input3 = browser.find_element(By.CSS_SELECTOR, '.first_block .form-control.third')
+        input3.send_keys("Smolensk")
+
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        welcome_text = welcome_text_elt.text
+
+        assert welcome_text == "Congratulations! You have successfully registered!"
+
+if __name__ == '__main__':
+    pytest.main()
+
